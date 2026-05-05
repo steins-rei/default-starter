dd38d5ddede93cf066458abae6f3111ac44cdffe
Steins <stienrei@gmail.com>
2026-05-05 10:48:09 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -11,31 +11,6 @@ const nextConfig: NextConfig = {
       },
     ]
   },
-
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: any) =>
-      rule.test?.test?.(".svg")
-    );
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, //
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-    config.resolve.alias['@styles'] = path.resolve(__dirname, 'styles');
-
-    return config;
-  },
 };
 
 export default nextConfig;
