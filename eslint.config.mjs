04e1b05551f0d068678e06a8b5e683161494facc
Steins <stienrei@gmail.com>
2026-05-05 10:22:20 +0800

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,17 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
@@ -14,5 +25,7 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
+
+export default eslintConfig;
 
 export default eslintConfig;
